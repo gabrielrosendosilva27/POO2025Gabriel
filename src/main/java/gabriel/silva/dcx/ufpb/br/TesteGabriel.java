f94c51f8d538ff4e3fc588ef910b30ad1f99603b
gabri <gabrielsrosendo@gmail.com>
2025-11-15 12:50:17 -0300

    ajeitei o teste

---
+++ src/main/java/gabriel/silva/dcx/ufpb/br/TesteGabriel.java
@@ -0,0 +1,10 @@
+package gabriel.silva.dcx.ufpb.br;
+
+public class TesteGabriel {
+    void main(){
+        IO.println("OI");
+        String nome = IO.readln("Digite seu nome: ");
+        IO.println("OI " + nome);
+
+    }
+}
